ef7eea704621f93f470e7fc3aff8f949a32a14e1
Muhammadsafarali <dr.safarali@yandex.ru>
2017-12-26 17:06:18 +0300

    first commit

---
+++ app/src/main/java/com/dubllik/mystlviewergles20/GLActivity.java
@@ -0,0 +1,29 @@
+package com.dubllik.mystlviewergles20;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class GLActivity extends Activity {
+
+    private GLSurfaceView mGLSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gl);
+
+        mGLSurfaceView = new MyGLSurfaceView(this);
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/com/dubllik/mystlviewergles20/Vector3f.java
@@ -0,0 +1,39 @@
+package com.dubllik.mystlviewergles20;
+
+import android.opengl.Matrix;
+
+public class Vector3f {
+
+    private static final float[] matrix = new float[16];
+    private static final float[] inVec = new float[4];
+    private static final float[] outVec = new float[4];
+
+    public float x,y,z;
+
+    public Vector3f (float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3f set(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        return this;
+    }
+
+    public Vector3f rotate(float angle, float axisX, float axisY, float axisZ) {
+        inVec[0] = x;
+        inVec[1] = y;
+        inVec[2] = z;
+        inVec[3] = 1;
+        Matrix.setIdentityM(matrix, 0);
+        Matrix.rotateM(matrix, 0, angle, axisX, axisY, axisZ);
+        Matrix.multiplyMV(outVec, 0, matrix, 0, inVec, 0);
+        x = outVec[0];
+        y = outVec[1];
+        z = outVec[2];
+        return this;
+    }
+}
